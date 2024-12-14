e9690a5f55f906489fad915be40191c00ee0419a
Tony <tonytimoshe@gmail.com>
2024-12-14 16:16:53 +0200

    implemented the get recipe function

--- main.go
+++ main.go
@@ -99,7 +99,41 @@ func (h *RecipesHandler) ListRecipes(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonBytes)
 }
-func (h *RecipesHandler) GetRecipe(w http.ResponseWriter, r *http.Request)    {}
+
+func (h *RecipesHandler) GetRecipe(w http.ResponseWriter, r *http.Request) {
+	// Extract the resource ID/slug using a regex
+	matches := RecipeReWithID.FindStringSubmatch(r.URL.Path)
+	// Expect matches to be length >= 2 (full string + 1 matching group)
+	if len(matches) < 2 {
+		InternalServerErrorHandler(w, r)
+		return
+	}
+
+	// Retrieve recipe from the store
+	recipe, err := h.store.Get(matches[1])
+	if err != nil {
+		// Special case of NotFound Error
+		if err == recipes.ErrNotFound {
+			NotFoundHandler(w, r)
+			return
+		}
+
+		// Every other error
+		InternalServerErrorHandler(w, r)
+		return
+	}
+
+	// Convert the struct into JSON payload
+	jsonBytes, err := json.Marshal(recipe)
+	if err != nil {
+		InternalServerErrorHandler(w, r)
+		return
+	}
+
+	// Write the results
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
 func (h *RecipesHandler) UpdateRecipe(w http.ResponseWriter, r *http.Request) {}
 func (h *RecipesHandler) DeleteRecipe(w http.ResponseWriter, r *http.Request) {}
 
