94e1545a8a04f5b87684fbe972775043b9bdd567
Tony <tonytimoshe@gmail.com>
2024-12-14 15:28:48 +0200

    Added recipes memory store and Error handlers

--- main.go
+++ main.go
@@ -8,10 +8,13 @@ import (
 )
 
 func main() {
+	store := recipes.NewMemStore()
+	recipesHandler := NewRecipesHandler(store)
+
 	mux := http.NewServeMux()
 	mux.Handle("/", &homeHandler{})
-	mux.Handle("/recipes", &recipesHandler{})
-	mux.Handle("/recipes/", &recipesHandler{})
+	mux.Handle("/recipes", recipesHandler)
+	mux.Handle("/recipes/", recipesHandler)
 
 	http.ListenAndServe(":8080", mux)
 }
@@ -22,14 +25,22 @@ func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("This is my home page"))
 }
 
-type recipesHandler struct{}
+type RecipesHandler struct {
+	store recipeStore
+}
+
+func NewRecipesHandler(s recipeStore) *RecipesHandler {
+	return &RecipesHandler{
+		store: s,
+	}
+}
 
 var (
 	RecipeRe       = regexp.MustCompile(`^/recipes/*$`)
 	RecipeReWithID = regexp.MustCompile(`^/recipes/([a-z0-9]+(?:-[a-z0-9]+)+)$`)
 )
 
-func (h *recipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *RecipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch {
 	case r.Method == http.MethodPost && RecipeRe.MatchString(r.URL.Path):
 		h.CreateRecipe(w, r)
@@ -51,11 +62,11 @@ func (h *recipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (h *recipesHandler) CreateRecipe(w http.ResponseWriter, r *http.Request) {}
-func (h *recipesHandler) ListRecipes(w http.ResponseWriter, r *http.Request)  {}
-func (h *recipesHandler) GetRecipe(w http.ResponseWriter, r *http.Request)    {}
-func (h *recipesHandler) UpdateRecipe(w http.ResponseWriter, r *http.Request) {}
-func (h *recipesHandler) DeleteRecipe(w http.ResponseWriter, r *http.Request) {}
+func (h *RecipesHandler) CreateRecipe(w http.ResponseWriter, r *http.Request) {}
+func (h *RecipesHandler) ListRecipes(w http.ResponseWriter, r *http.Request)  {}
+func (h *RecipesHandler) GetRecipe(w http.ResponseWriter, r *http.Request)    {}
+func (h *RecipesHandler) UpdateRecipe(w http.ResponseWriter, r *http.Request) {}
+func (h *RecipesHandler) DeleteRecipe(w http.ResponseWriter, r *http.Request) {}
 
 type recipeStore interface {
 	Add(name string, recipe recipes.Recipe) error
@@ -63,4 +74,14 @@ type recipeStore interface {
 	Update(name string, recipe recipes.Recipe) error
 	List() (map[string]recipes.Recipe, error)
 	Remove(name string) error
+}
+
+func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 Internal Server Error"))
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("404 Not Found"))
 }
